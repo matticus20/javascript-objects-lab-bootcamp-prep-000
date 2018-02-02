54d2b7b294ed4b55e5223680d5a27f6b3095b409
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-02-02 03:37:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return object.assign({}, obj, { [key]: value })
 }
 updateObjectWithKeyAndValue(recipes, 'flour', '1 cup')
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  
+}
