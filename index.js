f12aaee415eaff563ac6e2d296462f2f2e85f3a6
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-02-02 03:48:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return object.assign({}, obj, { [key]: value })
+  return object.assign({}, object, { [key]: value })
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value
-  return obj
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
 }
 destructivelyUpdateObjectWithKeyAndValue(recipes, 'vanilla', '1 tsp')
 
