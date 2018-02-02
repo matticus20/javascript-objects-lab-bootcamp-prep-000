1111f0387bb975e45beebe6154794578291410c5
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-02-02 03:49:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object
 }
